76551352f540f163544fe3e383d002ae7f661f2d
Brian Williams <email.brianwilliams@gmail.com>
2023-01-22 21:42:38 +0000

    Really EOD, need to rejig the state managment maybe introduce redux either way fix the crappy state managment on the json converter

--- src/renderer/Containers/JSONYamlConverter/index.tsx
+++ src/renderer/Containers/JSONYamlConverter/index.tsx
@@ -1,22 +1,20 @@
 import { useEffect, useState } from 'react';
-import { useDebounce } from 'usehooks-ts';
 import { BiFileBlank } from 'react-icons/bi';
 import { FaRegCopy } from 'react-icons/fa';
 import { IoCloseSharp } from 'react-icons/io5';
 import { MdContentPaste } from 'react-icons/md';
-import TextArea from 'renderer/Components/TextArea';
+import Editor from '@monaco-editor/react';
 import HeaderedSection from 'renderer/Components/HeaderedSection';
 import { useApi } from '../../../Contexts/ApiContext';
 import JSONYamlSettings, { ISettings } from './JSONYamlSettings';
 import './style.scss';
-import Editor from '@monaco-editor/react';
 
 // TODO: Add clipboard features and file loading
 // see: https://www.electronjs.org/docs/latest/tutorial/ipc
 // see: https://stackoverflow.com/questions/31130150/in-electron-framework-can-i-access-clipboard
 // const { clipboard } = require('electron');
-//setInput(clipboard.readText('clipboard')),
-//clipboard.writeText(getOutput(), 'clipboard'),
+// setInput(clipboard.readText('clipboard')),
+// clipboard.writeText(getOutput(), 'clipboard'),
 
 export default function JWTEncoder() {
 	const api = useApi();
@@ -24,8 +22,6 @@ export default function JWTEncoder() {
 	const [yaml, setYaml] = useState('');
 	const [input, setInput] = useState('');
 	const [settings, setSettings] = useState({} as ISettings);
-
-	const debouncedInput = useDebounce<string>(input, 300);
 
 	const didSettingsUpdate = (value: ISettings) => {
 		setSettings(value);
@@ -51,12 +47,6 @@ export default function JWTEncoder() {
 		}
 	};
 
-	const clearInput = () => {
-		setInput('');
-		setJSON('');
-		setYaml('');
-	};
-
 	const getInputLanguage = (): string => {
 		switch (settings.conversion) {
 			default:
@@ -77,21 +67,46 @@ export default function JWTEncoder() {
 		}
 	};
 
-	// useEffect(() => {
-	// 	switch (settings.conversion) {
-	// 		default:
-	// 		case 'JSONToYaml':
-	// 			setJSON(input);
-	// 			break;
-	// 		case 'YamlToJSON':
-	// 			setYaml(input);
-	// 			break;
-	// 	}
-	// }, [input, settings.conversion, settings.indent]);
+	const inputHeaderItems = [
+		{
+			label: 'Paste',
+			icon: MdContentPaste,
+			action: () => console.log('wooter'),
+		},
+		{
+			icon: BiFileBlank,
+			action: () => console.log('wooter'),
+		},
+
+		{
+			icon: IoCloseSharp,
+			action: () => setInput(''),
+		},
+	];
+
+	const outputHeaderItems = [
+		{
+			label: 'Copy',
+			icon: FaRegCopy,
+			action: () => console.log('wooter'),
+		},
+	];
+
+	useEffect(() => {
+		switch (settings.conversion) {
+			default:
+			case 'JSONToYaml':
+				setJSON(input);
+				break;
+			case 'YamlToJSON':
+				setYaml(input);
+				break;
+		}
+	}, [input, settings.conversion, settings.indent]);
 
 	useEffect(() => {
 		const output = api.jsonYamlConverterApi.convert(
-			debouncedInput,
+			input,
 			settings.conversion,
 			settings.indent
 		);
@@ -105,12 +120,7 @@ export default function JWTEncoder() {
 				setJSON(output);
 				break;
 		}
-	}, [
-		api.jsonYamlConverterApi,
-		debouncedInput,
-		settings.conversion,
-		settings.indent,
-	]);
+	}, [api.jsonYamlConverterApi, input, settings.conversion, settings.indent]);
 
 	return (
 		<div className="json-yaml-converter">
@@ -120,27 +130,9 @@ export default function JWTEncoder() {
 				/>
 			</HeaderedSection>
 			<div className="form">
-				<HeaderedSection
-					title="Input"
-					items={[
-						{
-							label: 'Paste',
-							icon: MdContentPaste,
-							action: () => console.log('wooter'),
-						},
-						{
-							icon: BiFileBlank,
-							action: () => console.log('wooter'),
-						},
-
-						{
-							icon: IoCloseSharp,
-							action: () => clearInput(),
-						},
-					]}
-				>
+				<HeaderedSection title="Input" items={inputHeaderItems}>
 					<Editor
-						height="300px"
+						height="350px"
 						language={getInputLanguage()}
 						theme="vs-dark"
 						options={{
@@ -149,23 +141,10 @@ export default function JWTEncoder() {
 						}}
 						onChange={(e: any) => setInput(e)}
 					/>
-					{/* <TextArea
-						height="500px"
-						onChange={(text: string) => setInput(text)}
-					/> */}
 				</HeaderedSection>
-				<HeaderedSection
-					title="Output"
-					items={[
-						{
-							label: 'Copy',
-							icon: FaRegCopy,
-							action: () => console.log('wooter'),
-						},
-					]}
-				>
+				<HeaderedSection title="Output" items={outputHeaderItems}>
 					<Editor
-						height="300px"
+						height="350px"
 						language={getOutputLanguage()}
 						theme="vs-dark"
 						options={{
@@ -175,7 +154,6 @@ export default function JWTEncoder() {
 						}}
 						value={getOutput()}
 					/>
-					{/* <TextArea text={getOutput()} height="500px" disabled /> */}
 				</HeaderedSection>
 			</div>
 		</div>
